9a3e6df6024d9c722d84174ecd6718cb092b49ee
nalexand <alxdthn@gmail.com>
2019-12-08 18:36:10 +0300

    clean

--- app/src/main/java/com/nalexand/friendlocation/di/modules/ViewModelModule.kt
+++ app/src/main/java/com/nalexand/friendlocation/di/modules/ViewModelModule.kt
@@ -1,35 +1,22 @@
 package com.nalexand.friendlocation.di.modules
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.nalexand.friendlocation.di.ViewModelFactory
-import com.nalexand.friendlocation.di.key.ViewModelKey
 import com.nalexand.friendlocation.ui.add_user.AddUserViewModel
 import com.nalexand.friendlocation.ui.home.HomeViewModel
 import com.nalexand.friendlocation.ui.notes.NotesViewModel
 import dagger.Binds
 import dagger.Module
-import dagger.multibindings.IntoMap
 
 @Suppress("unused")
 @Module
 abstract class ViewModelModule {
 
 	@Binds
-	internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-
-	@Binds
-	@IntoMap
-	@ViewModelKey(HomeViewModel::class)
 	internal abstract fun bindHomeViewModel(viewModel: HomeViewModel): ViewModel
 
 	@Binds
-	@IntoMap
-	@ViewModelKey(AddUserViewModel::class)
 	internal abstract fun bindAddUserViewModel(viewModel: AddUserViewModel): ViewModel
 
 	@Binds
-	@IntoMap
-	@ViewModelKey(NotesViewModel::class)
 	internal abstract fun bindNotesViewModel(viewModel: NotesViewModel): ViewModel
 }
